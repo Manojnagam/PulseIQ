d9b36c986dfc4023fca88010b0028a184d9df81e
Manojnagam <nagam.kumar@learner.manipal.edu>
2026-05-20 08:07:21 +0530

    Fix asset routing - use ASSETS binding directly

--- pulsezen/router.js
+++ pulsezen/router.js
@@ -16,9 +16,6 @@ export default {
       }
     }
 
-    // For all other requests (images, CSS, JS etc.) fetch from main workers URL
-    const assetUrl = new URL(request.url);
-    assetUrl.hostname = 'pulsezen.nagam-kumar.workers.dev';
-    return env.ASSETS.fetch(assetUrl.toString());
+    return env.ASSETS.fetch(request);
   }
 };
